aa00afeeaf3e84fe909472b56ac322eb7a82494b
luca <luca.carpella@gmail.com>
2023-12-11 12:42:43 +0100

    Added first signatures and comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+pub mod tile_mapper {
+    use robotics_lib::interface::Tools;
 
+    pub struct TileMapper {}
+
+    impl Tools for TileMapper {}
+
+    /// This tool stores the number of elements found in any tile by the robot
+    /// It returns a `HashMap` where `key` is the element searched and `value` is a vector of tuples,
+    /// The tuple stores the coordinates of a tile and the number of elements cointained in that tile
+
+
+    impl TileMapper {
+
+    }
 }
+
+
+fn main() {}
