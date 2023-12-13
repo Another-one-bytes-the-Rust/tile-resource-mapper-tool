b378ead0f16db348b6dae9217f2132e4672cc2cc
luca <luca.carpella@gmail.com>
2023-12-13 15:43:59 +0100

    Added Coordinates and ContentQuantity types for more readability and efficiency. Also added two function signatures

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod tile_mapper {
     use std::collections::HashMap;
     use std::ops::Range;
     use robotics_lib::interface::{robot_map, Tools};
+    use robotics_lib::runner::{Robot, Runnable};
     use robotics_lib::world::tile::{Content, Tile};
     use robotics_lib::world::World;
 
@@ -10,16 +11,19 @@ pub mod tile_mapper {
 
     impl Tools for TileMapper {}
 
+    type Coordinates = (usize, usize);
+    type ContentQuantity = (Option<usize>, Option<Range<usize>>);
+
     impl TileMapper {
 
         /// The `collection` function stores the number of elements found in any tile discovered by the robot
         /// It returns a `HashMap` where `key` is the element searched and `value` is a vector of tuples,
         /// The tuple stores the coordinates of a tile in another tuple, and the number of elements contained in that tile
 
-        fn collection(&self, world: &World) -> Option<HashMap<Content, Vec<((usize, usize) , (Option<usize>, Option<Range<usize>>))>>> {
+        fn collection(world: &World) -> Option<HashMap<Content, Vec<(Coordinates, ContentQuantity)>>> {
 
             // HashMap instantiation
-            let mut object_list: HashMap<Content, Vec<((usize, usize) , (Option<usize>, Option<Range<usize>>))>> = HashMap::new();
+            let mut object_list: HashMap<Content, Vec<(Coordinates, ContentQuantity)>> = HashMap::new();
 
             // check whether the world has been already discovered or not
             match robot_map(&world) {
@@ -45,12 +49,12 @@ pub mod tile_mapper {
             Some(object_list)
         }
 
-        fn insert_in_map(tile: &Tile , list: &mut HashMap<Content, Vec<((usize, usize) , (Option<usize>, Option<Range<usize>>))>>, row: usize, col: usize) {
+        fn insert_in_map(tile: &Tile , list: &mut HashMap<Content, Vec<(Coordinates, ContentQuantity)>>, row: usize, col: usize) {
 
             /// This function inserts the coordinates of a `tile` and the number of elements in that `tile`
 
-            let coord = (row, col);
-            let value = tile.content.get_value();
+            let coord: Coordinates = (row, col);
+            let value: ContentQuantity = tile.content.get_value();
             let content = tile.content.clone();
 
             // if no tile with `content` is in the list, it creates a new entry with that keyword
@@ -59,27 +63,18 @@ pub mod tile_mapper {
                 .and_modify(|v| v.push((coord, value.clone())))
                 .or_insert(vec![(coord, value)]);
         }
+
+        fn find_closest(world: &World, robot: & impl Runnable) -> Coordinates {
+            let map = TileMapper::collection(world);
+            todo!()
+        }
+
+        fn find_most_loaded(world: &World) -> (Coordinates, ContentQuantity) {
+            let map = TileMapper::collection(world);
+            todo!()
+        }
     }
 }
 
 
-
-fn main() {
-    println!("============================");
-    println!("PLEASE BUY ME, UNCLE PEAR :)");
-    println!("============================\n");
-    println!("               /¯¯¯____");
-    println!("              /----¯¯¯");
-    println!("             /");
-    println!("          ^^ ^^");
-    println!("         //   \\\\");
-    println!("        //     \\\\");
-    println!("      //         \\\\");
-    println!("    //             \\\\");
-    println!("    //             \\\\");
-    println!("    //             \\\\");
-    println!("     //           \\\\");
-    println!("      //         \\\\");
-    println!("       \\|||||||||/");
-
-}
+fn main() {}
